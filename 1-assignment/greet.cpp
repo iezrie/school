78905e37b7ecf18da41f8fb0c33cc52ae521ecb9
Ezrie Madison <iezrie@gmail.com>
2021-01-20 18:36:46 -0600

    Week 1 Finished

---
+++ 1-assignment/greet.cpp
@@ -0,0 +1,12 @@
+// Ezrie Ward
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, Ezrie." << endl;
+    system("PAUSE");
+    return 0;
+}
---
+++ 2-assignment/initial.cpp
@@ -0,0 +1,17 @@
+// Ezrie Ward
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << " ********  ****   ****  *         *  " << endl;
+    cout << " *         *   * *   *  *         *  " << endl;
+    cout << " ******    *    *    *  *    *    *  " << endl;
+    cout << " *         *         *  *   * *   *  " << endl;
+    cout << " ********  *         *  ****   ****  " << endl;
+
+    system("PAUSE");
+    return 0;
+}
